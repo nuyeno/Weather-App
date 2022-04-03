4ac8acbb76669c330228aaccf5c65ad4e4c85ebd
nuyeno <101528194+nuyeno@users.noreply.github.com>
2022-04-02 22:00:46 -1000

    added forecast API, update current weather CSS

--- src/index.js
+++ src/index.js
@@ -24,24 +24,41 @@ if (currentMinutes < 10) {
 }
 h2.innerHTML = `${currentDay} ${currentHours}:${currentMinutes}`;
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Wed", "Thurs", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
                 <div class="card">
-                  <div class="weather-icon">☀️</div>
+                  <div class="weather-icon"><img src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png" alt="forecast-icon"/></div>
                   <div class="card-body">
                     <p class="card-text week-weather">
-                      ${day}
+                      ${formatDay(forecastDay.dt)}
                       <br />
-                      <span class="forecast-high">79°</span> <span class="forecast-low">68°</span>
+                      <span class="forecast-high">${Math.round(
+                        forecastDay.temp.max
+                      )}°</span> <span class="forecast-low">${Math.round(
+          forecastDay.temp.min
+        )}°</span>
                     </p>
                   </div>
                 </div>`;
+    }
     forecastHTML = forecastHTML + `</div>`;
   });
   forecastElement.innerHTML = forecastHTML;
